8dd0b5dbbbd63629101dcd972c50ec8361368e24
Sung J Byun <sungjbyun@D-173-250-158-112.dhcp4.washington.edu>
2017-01-13 18:33:02 -0800

    working on tasks.go

---
+++ controllers/tasks.go
@@ -0,0 +1,6 @@
+package controllers
+
+import (
+  "net/http"
+)
+func Create(w http.ResponseWriter, r *http.Request){}
---
+++ routers/task.go
@@ -0,0 +1,10 @@
+package routers
+
+func SetTaskRouters(router *mux.Router) *mux.Router{
+  router.HandleFunc("/tasks", controllers.Tasks.Create).Methods("POST")
+  router.HandleFunc("/tasks", controllers.Tasks.Get).Methods("GET")
+  router.HandleFunc("/tasks/{id}", controllers.Tasks.Show).Methods("GET")
+  router.HandleFunc("/tasks/{id}", controllers.Tasks.Update).Methods("PUT")
+  router.HandleFunc("/tasks/{id}", controllers.Tasks.Delete).Methods("DELETE")
+  return router
+}
